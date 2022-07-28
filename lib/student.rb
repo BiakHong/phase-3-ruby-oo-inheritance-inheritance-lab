725036f3f6098ebf9da2c3c5809cce09f4c3433e
Lian <biakhong@gmail.com>
2022-07-28 11:40:00 -0400

    commit tree 9fadec722da57e45768ac718796a701713266301

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+    def learn(subjects)
+        self.knowledge.append(subjects)
+
+    end
+    def knowledge
+        @knowledge
+    end
 
 end
